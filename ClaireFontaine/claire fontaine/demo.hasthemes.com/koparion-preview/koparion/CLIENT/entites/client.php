295233d218e5e533e12ec5c595e0b4ee7f022c61
ahmedkolsi98 <ahmedkolsi98@gmail.com>
2019-12-03 01:01:52 +0100

    Backend prếte

---
+++ ClaireFontaine/claire fontaine/demo.hasthemes.com/koparion-preview/koparion/CLIENT/entites/client.php
@@ -0,0 +1,61 @@
+<?PHP
+class client{
+	private $id_client;
+	private $nom_client;
+	private $prenom_client;
+	private $email_client;
+	private $pwd_client;
+	private $tel_client;
+	private $categorie_client;
+	function __construct($id_client,$nom_client,$prenom_client,$email_client,$pwd_client,$tel_client,$categorie_client){
+		$this->id_client=$id_client;
+		$this->nom_client=$nom_client;
+		$this->prenom_client=$prenom_client;
+		$this->email_client=$email_client;
+		$this->pwd_client=$pwd_client;
+		$this->tel_client=$tel_client;
+		$this->categorie_client=$categorie_client;
+	}
+	function getidclient(){
+		return $this->id_client;
+	}
+	function getnomclient(){
+		return $this->nom_client;
+	}
+	function getprenomclient(){
+		return $this->prenom_client;
+	}
+	function getemailclient(){
+		return $this->email_client;
+	}
+	function getpwdclient(){
+		return $this->pwd_client;
+	}
+	function gettelclient(){
+		return $this->tel_client;
+	}
+	function getcategorieclient(){
+		return $this->tel_client;
+	}
+	function setnomclient($nom_client){
+		$this->nom_client=$nom_client;
+	}
+	function setprenomclient($prenom_client){
+		$this->prenom_client=$prenom_client;
+	}
+	function setemailclient($email_client){
+		$this->email_client;
+	}
+	function setpwdclient($pwd_client){
+		$this->pwd_client;
+	}
+	function settelclient($tel_client){
+		$this->tel_client;
+	}
+	function setcategorieclient($categorie_client){
+		$this->categorie_client;
+	}
+	
+}
+
+?>

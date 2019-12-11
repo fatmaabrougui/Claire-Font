06f17c5d36d392a3fe5f27711df616123fb56339
fatmaabrougui <47749544+fatmaabrougui@users.noreply.github.com>
2019-12-11 19:48:48 +0100

    Update shop.php

--- Views/Frontoffice/shop.php
+++ Views/Frontoffice/shop.php
@@ -7,7 +7,7 @@
                 public static function getConnexion() {
                   if (!isset(self::$instance)) {
                     try{
-                    self::$instance = new PDO('mysql:host=localhost;dbname=claire_fontaine', 'root', '');
+                    self::$instance = new PDO('mysql:host=localhost;dbname=clairefontaine', 'root', '');
                     self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                     }catch(Exception $e){
                         die('Erreur: '.$e->getMessage());
